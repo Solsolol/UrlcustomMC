0acf186ee42f66433c98d29a2ef2a93c7d0f181a
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-04-04 11:12:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ submodules.forEach((sm) => sm(app, {
     rootDirectory: __dirname,
 }));
 
+// Après vos middlewares existants
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    next();
+});
+
+// Ajoutez une route de test
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
 app.listen(app.get('port'), function() {
     console.log(`Express is running at localhost: ${app.get('port')}`);
 });
