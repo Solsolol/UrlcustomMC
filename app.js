7b5cb984b469e04cea8348267795ecea9218cd1e
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-04-04 13:45:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 
+// Add this before other routes
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong');
+});
+
 const submodules = [
     require('./modules/discount-code/app/app'),
     require('./modules/discount-redemption-split/app/app'),
@@ -9,11 +14,15 @@ const submodules = [
 
 const app = express();
 
-// Add CORS headers
+// Add these headers before other middleware
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://*.exacttarget.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
     next();
 });
 
@@ -36,9 +45,13 @@ app.use((req, res, next) => {
     next();
 });
 
-// Ajoutez une route de test
-app.get('/ping', (req, res) => {
-    res.send('pong');
+// Add this at the end of your app.js file
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: 'Internal Server Error',
+        message: process.env.NODE_ENV === 'development' ? err.message : undefined
+    });
 });
 
 app.listen(app.get('port'), function() {
