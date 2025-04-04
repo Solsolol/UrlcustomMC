a6481e9dbe119a159769bee3b863139a9c41cbc2
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-04-04 11:48:56 +0200

    Update config-json.js

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -5,11 +5,7 @@ module.exports = function configJSON(req) {
             isConfigured: true,
             icon: 'images/icon.svg',
             category: 'customer',
-            configurationPath: 'modules/discount-code/index.html',
-            edit: {
-                width: 500,
-                height: 400
-            }
+            configurationPath: '/modules/discount-code/index.html'
         },
         type: 'REST',
         lang: {
@@ -22,10 +18,9 @@ module.exports = function configJSON(req) {
             execute: {
                 inArguments: [
                     {
-                        discount: 10
-                    },
-                    {
-                        contactIdentifier: '{{Contact.Key}}'
+                        discount: 10,
+                        contactKey: '{{Contact.Key}}',
+                        emailAddress: '{{Contact.Default.Email}}'
                     }
                 ],
                 outArguments: [{
@@ -41,6 +36,23 @@ module.exports = function configJSON(req) {
                     }
                 }],
                 url: `https://custom-activity-sophia-sb-245958bcba3c.herokuapp.com/modules/discount-code/execute`
+            }
+        },
+        configurationArguments: {
+            publish: {
+                url: `https://custom-activity-sophia-sb-245958bcba3c.herokuapp.com/modules/discount-code/publish`
+            },
+            validate: {
+                url: `https://custom-activity-sophia-sb-245958bcba3c.herokuapp.com/modules/discount-code/validate`
+            },
+            stop: {
+                url: `https://custom-activity-sophia-sb-245958bcba3c.herokuapp.com/modules/discount-code/stop`
+            }
+        },
+        userInterfaces: {
+            configModal: {
+                height: 400,
+                width: 500
             }
         }
     };
